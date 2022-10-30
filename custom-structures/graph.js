39d3648f061af40426f86c5c55a5cff3d7535f3d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-30 16:12:59 +0200

    Implement display graph and has edge functions

--- custom-structures/graph.js
+++ custom-structures/graph.js
@@ -21,6 +21,19 @@ class Graph {
         this.adjacencyList[vertexOne].add(vertexTwo);
         this.adjacencyList[vertexTwo].add(vertexOne);
     }
+
+    hasEdge(vertexOne, vertexTwo) {
+        return (
+            this.adjacencyList[vertexOne].has(vertexTwo) &&
+            this.adjacencyList[vertexTwo].has(vertexOne)
+        )
+    }
+
+    display() {
+        for (const vertex in this.adjacencyList) {
+            console.log(vertex + ' -> ' + [...this.adjacencyList[vertex]]);
+        }
+    }
 }
 
 const graph = new Graph();
@@ -30,3 +43,6 @@ graph.addVertex('C');
 
 graph.addEdge('A', 'B');
 graph.addEdge('B', 'C');
+
+graph.display();
+console.log(graph.hasEdge('A', 'C'));
